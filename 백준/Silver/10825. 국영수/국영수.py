439697576020971423d0cb8c6d01dd97b16f77ac
JeongHanLim <40853979+lim-it-err@users.noreply.github.com>
2023-07-30 18:44:41 +0900

    [Silver IV] Title: 국영수, Time: 4548 ms, Memory: 86852 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,8 @@
+N = int(input())
+students = []
+for i in range(N):
+    students.append(input().split())
+students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+for student in students:
+    print(student[0])
+
